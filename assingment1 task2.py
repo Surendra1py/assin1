ad02debc83bea8e08b3284904c6a288f4b64b659
Surendra1py <SURENDAR1GANGWAR@GMAIL.COM>
2025-08-31 10:49:03 +0530

    Add files via upload

---
+++ assingment1 task2.py
@@ -0,0 +1,8 @@
+
+str1="John"
+str2="Doe"
+result=(str1+" "+str2)
+print('Enter your First name:',str1)
+print('Enter your last name:',str2)
+
+print("Hello,",result,"! welcome to the python program")
